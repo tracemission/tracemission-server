1bbd0dce41cccf1ee1099dcbc530cb79c1cda1db
DamienJochim <dev@joch.im>
2020-03-21 02:15:34 +0100

    Initial project setup

---
+++ src/test/java/org/wirvsvirushackathon/NativePersonResourceIT.java
@@ -0,0 +1,9 @@
+package org.wirvsvirushackathon;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativePersonResourceIT extends PersonResourceTest {
+
+    // Execute the same tests but in native mode.
+}
---
+++ src/test/java/org/wirvsvirushackathon/PersonResourceTest.java
@@ -0,0 +1,21 @@
+package org.wirvsvirushackathon;
+
+import io.quarkus.test.junit.QuarkusTest;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.is;
+
+@QuarkusTest
+public class PersonResourceTest {
+
+    @Test
+    public void testHelloEndpoint() {
+        given()
+          .when().get("/person")
+          .then()
+             .statusCode(200)
+             .body(is("hello"));
+    }
+
+}
