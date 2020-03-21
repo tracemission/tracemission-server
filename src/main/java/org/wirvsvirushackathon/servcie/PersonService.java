947ae5370b0efd8db533ca0a4384bf0a979aa2ad
DamienJochim <dev@joch.im>
2020-03-21 16:41:52 +0100

    Finish person endpoint

--- src/main/java/org/wirvsvirushackathon/servcie/PersonService.java
+++ src/main/java/org/wirvsvirushackathon/servcie/PersonService.java
@@ -1,28 +1,46 @@
 package org.wirvsvirushackathon.servcie;
 
+import org.neo4j.driver.Driver;
+import org.neo4j.driver.Values;
+import org.neo4j.driver.async.AsyncSession;
+import org.neo4j.driver.async.ResultCursor;
 import org.wirvsvirushackathon.model.Person;
+import org.wirvsvirushackathon.persistence.PersonQuery;
 
+import javax.inject.Inject;
 import javax.inject.Singleton;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.UUID;
+import java.util.concurrent.CompletionStage;
+
 
 @Singleton
 public class PersonService {
 
-    private Map<Long, Person> persons = new HashMap<>();
+    @Inject
+    Driver driver;
 
-    public void registerPerson(Person person) {
-        person.setId(getRandomId());
-        persons.put(person.getId(), person);
+    public CompletionStage<Person> registerPerson(Person person) {
+        AsyncSession session = driver.asyncSession();
+        return session
+                .writeTransactionAsync(tx -> tx
+                        .runAsync(PersonQuery.CREATE_QUERY, PersonQuery.getParameterMap(person))
+                        .thenCompose(ResultCursor::singleAsync)
+                )
+                .thenApply(record -> Person.from(record.get("p").asNode()))
+                .thenCompose(persistedPerson -> session.closeAsync().thenApply(signal -> persistedPerson));
     }
 
-    public Person getPersonById(long id){
-        return persons.get(id);
-    }
-
-    private long getRandomId() {
-        return 100000000000000L + (long) (Math.random() * (999999999999999L - 10000000000L));
+    public CompletionStage<Person> getPersonById(UUID id) {
+        AsyncSession session = driver.asyncSession();
+        return session
+                .writeTransactionAsync(tx -> tx
+                        .runAsync(PersonQuery.SELECT_ID_QUERY, Values.parameters(Person.ID_PROP, id.toString()))
+                        .thenCompose(ResultCursor::singleAsync)
+                )
+                .thenApply(record -> Person.from(record.get("p").asNode()))
+                .thenCompose(persistedPerson -> session.closeAsync().thenApply(signal -> persistedPerson));
     }
 
 }
