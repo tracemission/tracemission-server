c43348115fc4d9d1b229bdfb204d3bdddc829f11
s8jjzimm <s8jjzimm@stud.uni-saarland.de>
2020-03-21 17:29:46 +0100

    CheckInOut with UUIDs

--- src/main/java/org/wirvsvirushackathon/model/CheckInOutData.java
+++ src/main/java/org/wirvsvirushackathon/model/CheckInOutData.java
@@ -2,6 +2,7 @@ package org.wirvsvirushackathon.model;
 
 import javax.validation.constraints.NotBlank;
 import java.util.Date;
+import java.util.UUID;
 
 public class CheckInOutData {
 
@@ -10,31 +11,31 @@ public class CheckInOutData {
     private static final String TIMESTAMP_PROP = "timestamp";
 
     @NotBlank(message = "Person ID should not be blank.")
-    private long personId;
+    private UUID personId;
     @NotBlank(message = "Store ID should not be blank.")
-    private long storeId;
+    private UUID storeId;
     @NotBlank(message = "Timestamp should not be blank.")
     private Date timestamp;
 
-    public CheckInOutData(long personId, long storeId, Date timestamp) {
+    public CheckInOutData(UUID personId, UUID storeId, Date timestamp) {
         this.personId = personId;
         this.storeId = storeId;
         this.timestamp = timestamp;
     }
 
-    public long getPersonId() {
+    public UUID getPersonId() {
         return personId;
     }
 
-    public void setPersonId(long personId) {
+    public void setPersonId(UUID personId) {
         this.personId = personId;
     }
 
-    public long getStoreId() {
+    public UUID getStoreId() {
         return storeId;
     }
 
-    public void setStoreId(long storeId) {
+    public void setStoreId(UUID storeId) {
         this.storeId = storeId;
     }
 
